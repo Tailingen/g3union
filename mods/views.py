276290a977e746f55ac55633c079bf9dc21c0fa5
Vladislav <Tailingen@mail.ru>
2025-03-29 14:49:02 +0500

    add titel-pages

--- mods/views.py
+++ mods/views.py
@@ -11,9 +11,10 @@ class Home(ListView):
     template_name = 'mods/home.html'
     context_object_name = 'posts'
     paginate_by = 10
+    extra_context = {'title': 'Список модов'}
 
 def about(request):
-    return render(request, 'mods/about.html')
+    return render(request, 'mods/about.html', {'title': 'О сайте'})
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h2>Такой страницы не существует, глупый морра</h2>')
@@ -22,6 +23,7 @@ class ShowPost(DetailView):
     model = Mod
     template_name = 'mods/mod.html'
     context_object_name = 'post'
+    extra_context = {'title': 'X'}
 
 class CreatePost(CreateView):
     model = Mod
@@ -29,18 +31,21 @@ class CreatePost(CreateView):
     template_name = 'mods/create.html'
     #fields = ['slug', 'name', 'desc', 'content', 'is_published', 'category']
     success_url = reverse_lazy('home')
+    extra_context = {'title': 'Создать пост'}
 
 class UpdatePost(UpdateView):
     model = Mod
     template_name = 'mods/update.html'
     fields = ['name', 'desc', 'content', 'is_published', 'category']
     success_url = reverse_lazy('home')
+    extra_context = {'title': 'Редактировать пост'}
 
 class ShowCategory(ListView):
     model = Mod
     template_name = 'mods/category.html'
     context_object_name = 'posts'
     paginate_by = 10
+    extra_context = {'title': 'статьи по категориям'}
 
     def get_queryset(self):
         return Mod.objects.filter()
